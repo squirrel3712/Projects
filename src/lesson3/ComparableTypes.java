9e040e08eca8b10c6511482ceb5b29a588822995
Ann Chunosova <squirrel3712@gmail.com>
2017-07-01 20:37:15 +0300

    lesson3

---
+++ src/lesson3/ComparableTypes.java
@@ -0,0 +1,80 @@
+package lesson3;
+
+public class ComparableTypes {
+    byte b = -126;
+    short s = -32000;
+    int i = 56086743;
+    long l = 3L;
+    float f = 1.1f;
+    double d = 0.2;
+    char c = 'a';
+    boolean bool = false;
+
+    public void toByte(){
+        b = (byte)s;
+        b = (byte)i;
+        b = (byte)l;
+        b = (byte)f;
+        b = (byte)d;
+        b = (byte)c;
+    }
+
+    public void toShort(){
+        s = b;
+        s = (short)i;
+        s = (short)l;
+        s = (short)f;
+        s = (short)d;
+        s = (short)c;
+    }
+
+    public void toInt(){
+        i = b;
+        i = s;
+        i = (int)l;
+        i = (int)f;
+        i = (int)d;
+        i = c;
+    }
+
+    public void toLong(){
+        l = b;
+        l = s;
+        l = i;
+        l = (long)f;
+        l = (long)d;
+        l = c;
+    }
+
+    public void toFloat(){
+        f = b;
+        f = s;
+        f = i;
+        f = l;
+        f = (float)d;
+        f = c;
+    }
+
+    public void toDouble(){
+        d = b;
+        d = s;
+        d = i;
+        d = l;
+        d = f;
+        d = c;
+    }
+
+    public void toChar(){
+        c = (char)b;
+        c = (char)s;
+        c = (char)i;
+        c = (char)l;
+        c = (char)f;
+        c = (char)d;
+    }
+
+    public static void main(String[] args) {
+        ComparableTypes types = new ComparableTypes();
+        types.toByte();
+    }
+}
---
+++ src/lesson3/Literals.java
@@ -0,0 +1,24 @@
+package lesson3;
+
+public class Literals {
+    public static void main(String[] args) {
+        boolean b = true;
+        char ch = 'a';
+        String str = "Java";
+        int decimal = 22000;
+        long octal = 010L;
+        short binary = 0b101;
+        int hex = 0x0089807;
+        float f = .9f;
+        double d = 0.567;
+        System.out.println("Boolean literal: " + b);
+        System.out.println("Character literal: " + ch);
+        System.out.println("String literal: " + str);
+        System.out.println("Decimal literal: " + decimal);
+        System.out.println("Octal literal: " + octal);
+        System.out.println("Binary literal: " + binary);
+        System.out.println("Hexadecimal literal: " + hex);
+        System.out.println("Float literal: " + f);
+        System.out.println("Double literal: " + d);
+    }
+}
