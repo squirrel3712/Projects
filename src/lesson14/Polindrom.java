bf2be9ea1e776cf715a1870b55f68232bcf11e89
Ann Chunosova <squirrel3712@gmail.com>
2017-08-11 12:39:43 +0300

    fixed

--- src/lesson14/Polindrom.java
+++ src/lesson14/Polindrom.java
@@ -4,17 +4,20 @@ import java.util.regex.Pattern;
 
 public class Polindrom {
     public static void main(String[] args) {
-        findPolindrom("I love Java 4554 and 77 and others");
+        findPolindrom("I love Java 4554 and 77 and 12 others");
     }
 
     public static void findPolindrom(String s) {
+        String st = s.trim();
         Pattern pattern = Pattern.compile("\\D+");
-        String[] array = pattern.split(s);
+        String[] array = pattern.split(st);
         StringBuilder builder;
         for (String str : array) {
             builder = new StringBuilder(str);
-            if (builder.equals(builder.reverse())) {
-                System.out.println(str);
+            if (str.equals(builder.reverse().toString())) {
+                if (!str.isEmpty()) {
+                    System.out.println(str);
+                }
             }
         }
     }
--- src/lesson14/RegexDemo.java
+++ src/lesson14/RegexDemo.java
@@ -5,8 +5,8 @@ import java.util.regex.Pattern;
 
 public class RegexDemo {
     public static void main(String[] args) {
-        String s = "Java 5, Java 6, Java 7, java 8";
-        Pattern p = Pattern.compile("Java\\s?\\d");
+        String s = "Java   5, Java6, Java 7, java 8";
+        Pattern p = Pattern.compile("Java\\s*\\d");
         Matcher matcher = p.matcher(s);
         while (matcher.find()) {
             System.out.println(matcher.group());
