6a3ae638630e0e669b51570c13caa01911e5e65a
Ann Chunosova <squirrel3712@gmail.com>
2017-09-12 12:36:40 +0300

    fixed

--- src/lesson19/task3/Horse.java
+++ src/lesson19/task3/Horse.java
@@ -6,12 +6,11 @@ public class Horse extends Animal implements Serializable {
     private Halter halter;
 
     public Horse() {
-        super();
         System.out.println("In constructor Horse");
     }
 
     public static void serialize(Horse horse) {
-        try (OutputStream os = new FileOutputStream("src/lesson19/io/output.txt");
+        try (OutputStream os = new FileOutputStream("src/io/output.txt");
              ObjectOutput out = new ObjectOutputStream(os)) {
             out.writeObject(horse);
         } catch (IOException e) {
@@ -19,18 +18,20 @@ public class Horse extends Animal implements Serializable {
         }
     }
 
-    public static void deSerialize(Horse horse) {
-        try (InputStream is = new FileInputStream("src/lesson19/io/output.txt");
+    public static Horse deSerialize() {
+        Horse horse = null;
+        try (InputStream is = new FileInputStream("src/io/output.txt");
              ObjectInput oi = new ObjectInputStream(is)) {
-            horse = (Horse) oi.readObject();
+          horse = (Horse) oi.readObject();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return horse;
     }
 
     public static void main(String[] args) {
         Horse horse = new Horse();
         serialize(horse);
-        deSerialize(horse);
+        horse = deSerialize();
     }
 }
